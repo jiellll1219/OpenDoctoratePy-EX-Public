3918b029f7e72602dcc48f8dfbe865730e71f611
MVXTP <61046104+jiellll1219@users.noreply.github.com>
2024-02-25 14:24:20 +0800

    更新building相关内容

--- server/building.py
+++ server/building.py
@@ -20,3 +20,80 @@ def buildingSync():
     }
     return data
 
+def buildingGetRecentVisitors():
+
+    data = request.data
+    data = {"num":0}
+    return data
+
+def buildingGetInfoShareVisitorsNum():
+
+    data = request.data
+
+    return data
+
+def buildingAssignChar():
+
+    data = request.data
+
+    return data
+
+def buildingChangeDiySolution():
+
+    data = request.data
+
+    return data
+
+def buildingChangeManufactureSolution():
+
+    data = request.data
+
+    return data
+
+def buildingSettleManufacture():
+
+    data = request.data
+
+    return data
+
+def buildingWorkshopSynthesis():
+
+    data = request.data
+
+    return data
+
+def buildingUpgradeSpecialization():
+
+    data = request.data
+
+    return data
+
+def buildingCompleteUpgradeSpecialization():
+
+    data = request.data
+
+    return data
+
+def buildingDeliveryOrder():
+
+    data = request.data
+
+    return data
+
+def buildingDeliveryBatchOrder():
+
+    data = request.data
+
+    return data
+
+def buildingCleanRoomSlot():
+
+    data = request.data
+
+    return data
+
+def buildinggetAssistReport():
+
+    data = request.data
+    
+    return data
